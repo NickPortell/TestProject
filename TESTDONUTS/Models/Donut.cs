e19e8c1cc819e12e8274a6489824eb0673ec03a5
Nick <nick.portell12@gmail.com>
2019-06-13 09:59:05 -0400

    Test Commit

---
+++ TESTDONUTS/Models/Donut.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TESTDONUTS.Models
+{
+    public class Donut
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string PhotoURL { get; set; }
+        public string[] Extras { get; set; }
+
+
+    }
+}
